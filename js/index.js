4de4a950437288b3b77d97c1af14a05d6475e867
Slava <plumsoftwareofficial@gmail.com>
2023-10-10 18:00:50 +0600

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,4 @@
+function regirectToRegistration() {
+    // window.open("./templates/registration.html", "_blank");
+    window.location.href = "./templates/registration.html";
+}
---
+++ js/registration.js
@@ -0,0 +1,25 @@
+function signUp() {
+    
+}
+
+document.querySelector('.sign_in_button').addEventListener('click', function() {
+    var email = document.querySelector('input[type="text"]').value;
+    var password = document.querySelector('input[type="password"]').value;
+    var confirmPassword = document.querySelectorAll('input[type="password"]')[1].value;
+    
+    // Проверка формата почты
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var isValidEmail = emailRegex.test(email);
+    
+    // Проверка совпадения паролей
+    var arePasswordsMatch = password === confirmPassword;
+    
+    // Вывод сообщений об ошибках
+    if (!isValidEmail) {
+       alert('Неверный формат почты!');
+    } else if (!arePasswordsMatch) {
+       alert('Пароли не совпадают!');
+    } else {
+       alert('Регистрация успешно завершена!');
+    }
+ });
