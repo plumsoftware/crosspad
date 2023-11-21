7559a427ac55addd5f5ddc8bb7a458b3f8ebc8de
Slava <plumsoftwareofficial@gmail.com>
2023-11-21 19:30:10 +0600

    [+] added new function to check data

--- js/index.js
+++ js/index.js
@@ -14,9 +14,38 @@ function signIn() {
 
     if (email === "" || password === "") {
         textField.style.borderColor = 'red';
+        alert("Неверный пароль или почта")
     } else if (!isValidEmail) {
         alert("Некорректный формат электронной почты");
     } else {
+        redirectToNotes(email, password)
+    }
+}
+
+function redirectToNotes(email, password) {
+    var savedEmail = localStorage.getItem("email")
+    var savedPassword = localStorage.getItem("password")
+
+    const emailTextField = document.getElementById("email_text_field");
+    const passwordTextField = document.getElementById("password_text_field");
+
+
+    var isEmailEqual = savedEmail === email
+    var isPasswordEqual = savedPassword === password
+
+    //Logs
+    console.log("email is " + savedEmail)
+    console.log("password is " + savedPassword)
+
+    if (isEmailEqual && isPasswordEqual) {
         window.location.href = "./templates/notes.html";
+    } else if (isEmailEqual && !isPasswordEqual) {
+        alert("Неверный пароль")
+        emailTextField.style.borderColor = 'red';
+    } else if (!isEmailEqual && isPasswordEqual) {
+        alert("Неверный пароль")
+        passwordTextField.style.borderColor = 'red';
+    } else if (!isEmailEqual && !isPasswordEqual){
+        alert("Неверный пароль и почта")
     }
 }
